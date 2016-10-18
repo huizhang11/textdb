564e9d123cf2acfe99eaa3ae78d2703de168a563
Kishore Narendran <kishore.narendran09@gmail.com>
2016-10-17 18:10:55 -0700

    Adds a Bean for the projeciton  operator

---
+++ textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/request/operatorbean/ProjectionBean.java
@@ -0,0 +1,12 @@
+package edu.uci.ics.textdb.web.request.operatorbean;
+
+import edu.uci.ics.textdb.web.request.Operator;
+
+/**
+ * This class defines the properties/data members specific to the Projection operator
+ * and extends the Operator class which defines the data members general to all operators
+ * Created by kishorenarendran on 10/17/16.
+ */
+public class ProjectionBean extends Operator {
+    // This will currently remain empty as the Projection operator currently does not contain any special properties
+}
