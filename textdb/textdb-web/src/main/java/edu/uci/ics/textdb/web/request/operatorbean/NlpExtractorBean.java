49827096c26e076c7ea32f495adfe46f46ee7419
Kishore Narendran <kishore.narendran09@gmail.com>
2016-10-17 18:00:31 -0700

    Adds a Bean for NlpExtractor

---
+++ textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/request/operatorbean/NlpExtractorBean.java
@@ -0,0 +1,33 @@
+package edu.uci.ics.textdb.web.request.operatorbean;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import edu.uci.ics.textdb.dataflow.nlpextrator.NlpPredicate.NlpTokenType;
+import edu.uci.ics.textdb.web.request.Operator;
+
+/**
+ * This class defines the properties/data members specific to the NlpExtractor operator
+ * and extends the Operator class which defines the data members general to all operators
+ * Created by kishorenarendran on 10/17/16.
+ */
+public class NlpExtractorBean extends Operator{
+    @JsonProperty("nlp_type")
+    private NlpTokenType nlpTokenType;
+
+    public NlpExtractorBean() {
+    }
+
+    public NlpExtractorBean(String operatorID, String operatorType, NlpTokenType nlpTokenType) {
+        super(operatorID, operatorType);
+        this.nlpTokenType = nlpTokenType;
+    }
+
+    @JsonProperty("nlp_type")
+    public NlpTokenType getNlpTokenType() {
+        return nlpTokenType;
+    }
+
+    @JsonProperty("nlp_type")
+    public void setNlpTokenType(NlpTokenType nlpTokenType) {
+        this.nlpTokenType = nlpTokenType;
+    }
+}
