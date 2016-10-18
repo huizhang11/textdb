3f133e957e388b64f3a9bca5f232f0248db90339
Kishore Narendran <kishore.narendran09@gmail.com>
2016-10-17 18:43:56 -0700

    Modifies necessary annotations for all beans:

--- textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/request/Operator.java
+++ textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/request/Operator.java
@@ -4,17 +4,30 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import edu.uci.ics.textdb.web.request.operatorbean.RegexMatcherBean;
+import edu.uci.ics.textdb.dataflow.sink.IndexSink;
+import edu.uci.ics.textdb.web.request.operatorbean.*;
 
 /**
  * This class is the abstract class that defines the data members common to all operators. It is
- * extended by invidual operator beans in order to define the data members specific to each
+ * extended by individual operator beans in order to define the data members specific to each
  * operator
  * Created by kishorenarendran on 10/12/16.
  */
 @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.PROPERTY, property="operator_type")
 @JsonSubTypes({
+        @JsonSubTypes.Type(value=DictionaryMatcherBean.class, name="DictionaryMatcher"),
+        @JsonSubTypes.Type(value=DictionarySourceBean.class, name="DictionarySource"),
+        @JsonSubTypes.Type(value=FileSinkBean.class, name="FileSink"),
+        @JsonSubTypes.Type(value=FuzzyTokenMatcherBean.class, name="FuzzyTokenMatcher"),
+        @JsonSubTypes.Type(value=FuzzyTokenSourceBean.class, name="FuzzyTokenSource"),
+        @JsonSubTypes.Type(value=IndexSinkBean.class, name="IndexSink"),
+        @JsonSubTypes.Type(value=JoinBean.class, name="Join"),
+        @JsonSubTypes.Type(value=KeywordMatcherBean.class, name="KeywordMatcher"),
+        @JsonSubTypes.Type(value=KeywordSourceBean.class, name="KeywordSource"),
+        @JsonSubTypes.Type(value=NlpExtractorBean.class, name="NlpExtractor"),
+        @JsonSubTypes.Type(value=ProjectionBean.class, name="Projection"),
         @JsonSubTypes.Type(value=RegexMatcherBean.class, name="RegexMatcher"),
+        @JsonSubTypes.Type(value=RegexSourceBean.class, name="RegexSource")
 })
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class Operator {
